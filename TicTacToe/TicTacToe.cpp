9a3087da2fabd21ee6e46ec95971112c89b5d02e
Tien Nguyen <tien.nguyen@edu.rptu.de>
2025-03-26 12:47:19 +0100

    Using vector for game

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -4,12 +4,59 @@
 
 using namespace std;
 
+
+int calculateWinner(const vector<vector<int>> &field) {
+    for (int i = 0; i <= 2; i++) {
+        if (field.at(i).at(0) != 0 &&
+            field.at(i).at(0) == field.at(i).at(1) &&
+            field.at(i).at(0) == field.at(i).at(2)) {
+            
+            return field.at(i).at(0);
+        }
+    }
+    
+    for (int i = 0; i <= 2; i++) {
+        if (field.at(0).at(i) != 0 &&
+            field.at(0).at(i) == field.at(1).at(i) &&
+            field.at(0).at(i) == field.at(2).at(i)) {
+            
+            return field.at(0).at(i);
+        }
+    }
+    
+    if (field.at(0).at(0) != 0 &&
+        field.at(0).at(0) == field.at(1).at(1) &&
+        field.at(0).at(0) == field.at(2).at(2)) {
+        
+        return field.at(0).at(0);
+    }
+    
+    if (field.at(0).at(2) != 0 &&
+        field.at(0).at(2) == field.at(1).at(1) &&
+        field.at(0).at(2) == field.at(2).at(0)) {
+        
+        return field.at(0).at(2);
+    }
+    
+    return 0;
+}
+
 int main(int argc, const char * argv[]){
 
-    vector<string> names;
-    names.push_back("Max");
-    names.push_back("Tien");
+    vector<vector<int>> field = {
+        {1, 0, 1},
+        {0, 1, 0},
+        {2, 0, 1}
+    };
 
-    cout << names[0] << endl;
+    for (const auto &r: field){
+        for (const int val: r){
+            cout << val << " "; 
+        }
+        cout << endl;
+    }
+
+    auto winner = calculateWinner(field);
+    cout << winner << " hat das Spiel gewonnen!" << endl;
     return 0;
 }
