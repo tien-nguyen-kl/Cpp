7bfb6a2cdca6b3aa0d910addfa71d98a3711621e
Tien Nguyen <tien.nguyen@edu.rptu.de>
2025-03-26 13:04:14 +0100

    TicTacToe by class

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,54 +1,89 @@
 #include <iostream>
-#include <array>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-int calculateWinner(const array<array<int,3>, 3>& field){
-    // Check for horizontal or vertical win
-    for (int i = 0; i < field.size(); i++){
-        // Check horizontal win (row-wise)
-        if (field[i][0] != 0 && field[i][0] == field[i][1] && field[i][0] == field[i][2]){
-            return field[i][0];
-        }
-        // Check vertical win (column-wise)
-        else if (field[0][i] != 0 && field[0][i] == field[1][i] && field[0][i] == field[2][i])
-        {
-            return field[0][i];
 
+class TicTacToeField{
+public:
+    TicTacToeField(){
+        cout <<"Ich werde aufgerufen, wenn die Klasse erstellt wird"<<endl;
+        field = {{
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        }};
+    };
+
+    void set_field(vector<vector<int>> field){
+        this->field = field;
+    }
+
+    void print_field(){
+        for (const auto &r: this->field){
+            for (const int val: r){
+                cout << val << " "; 
+            }
+            cout << endl;
         }
     }
-        // Check diagonal from top-left to bottom-right
-        if (field[0][0] != 0 && field[0][0] == field[1][1] && field[0][0] == field[2][2]){
-            return field[0][0];
-        }
-        // Check diagonal from top-right to bottom-left
-        if (field[2][0] != 0 && field[2][0] == field[1][1] && field[2][0] == field[0][2]){
-                return field[2][0];
-        }
 
-}
+    int calculateWinner() {
+        for (int i = 0; i <= 2; i++) {
+            if (this->field.at(i).at(0) != 0 &&
+            this->field.at(i).at(0) == this->field.at(i).at(1) &&
+            this->field.at(i).at(0) == this->field.at(i).at(2)) {
+                
+                return this->field.at(i).at(0);
+            }
+        }
+        
+        for (int i = 0; i <= 2; i++) {
+            if (this->field.at(0).at(i) != 0 &&
+            this->field.at(0).at(i) == this->field.at(1).at(i) &&
+                this->field.at(0).at(i) == this->field.at(2).at(i)) {
+                
+                return this->field.at(0).at(i);
+            }
+        }
+        
+        if (this->field.at(0).at(0) != 0 &&
+        this->field.at(0).at(0) == this->field.at(1).at(1) &&
+        this->field.at(0).at(0) == this->field.at(2).at(2)) {
+            
+            return this->field.at(0).at(0);
+        }
+        
+        if (this->field.at(0).at(2) != 0 &&
+        this->field.at(0).at(2) == this->field.at(1).at(1) &&
+        this->field.at(0).at(2) == this->field.at(2).at(0)) {
+            
+            return this->field.at(0).at(2);
+        }
+        
+        return 0;
+    }
+private:
+    vector<vector<int>> field;
+};
+
 
 int main(int argc, const char * argv[]){
 
-    array<array<int, 3>, 3> field = {{
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    }};
+    TicTacToeField field1;
 
-    field[0] = {1, 0, 1};
-    field[1] = {2, 2, 1};
-    field[2] = {1, 2, 1};
-    
-    int winner = calculateWinner(field);
+    vector<vector<int>> field = {
+        {1, 0, 1},
+        {2, 2, 1},
+        {1, 2, 1}
+    };
+
+    field1.set_field(field);
+    field1.print_field();
+    int winner = field1.calculateWinner();
     cout << winner << " hat das Spiel gewonnen" << endl;
 
-    for (const auto &r: field){
-        for (const int val: r){
-            cout << val << " "; 
-        }
-        cout << endl;
-    }
+
     return 0;
 }
